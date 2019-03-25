afe2e9d3e41da6a0adb6bc56b22a36954ed0bfc2
emersonbaria <emerson.baria@my.jcu.edu.au>
2019-03-25 21:10:41 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatch2/MainActivity.java
@@ -0,0 +1,61 @@
+package au.edu.jcu.cp3406.stopwatch2;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds=0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    //Start the Stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running=true;
+    }
+
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running=false;
+    }
+
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running=false;
+        seconds=0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours=seconds/3600;
+                int minutes=(seconds%3600)/60;
+                int secs=seconds%60;
+                String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+
+            }
+        });
+
+
+
+    }
+}
